4691c0acf61fce4084d68b830b1e477452d113ef
noodles <458404197@qq.com>
2022-04-11 08:32:13 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
+import styleImport, { VantResolve } from 'vite-plugin-style-import'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  plugins: [
+    viteCompression({
+      threshold: 10240
+    }),
+    vue(),
+    styleImport({
+      resolves: [VantResolve()]
+    })
+  ]
 })
